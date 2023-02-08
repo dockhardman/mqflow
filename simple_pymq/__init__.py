88588367465f7c91cd975bb623aac08f5d9aba60
Allen Chou <f1470891079@gmail.com>
2023-02-08 18:25:24 +0800

    TimeCounterProducer basic operation.

--- simple_pymq/__init__.py
+++ simple_pymq/__init__.py
@@ -1,10 +1,13 @@
 from .version import version
 from .broker.base import Broker, QueueBroker
+from .producer.base import Producer, TimeCounterProducer
 
 
 __version__ = version
 
 __all__ = [
     "Broker",
+    "Producer",
     "QueueBroker",
+    "TimeCounterProducer",
 ]
--- simple_pymq/producer/base.py
+++ simple_pymq/producer/base.py
@@ -1,12 +1,85 @@
+import asyncio
+import logging
 from abc import ABC
-from typing import Text, Type
+from numbers import Number
+from typing import Any, Optional, Text, Type
 
 from simple_pymq.broker.base import Broker
+from simple_pymq.config import settings
+from simple_pymq.exceptions import FullError
+
+
+logger = logging.getLogger(settings.logger_name)
 
 
 class Producer(ABC):
-    def __init__(self, name: Text = "Producer", *args, **kwargs):
+    def __init__(
+        self, name: Text = "Producer", *args, timeout: Optional[Number] = None, **kwargs
+    ):
         self.name = name
+        self.timeout = timeout
 
-    async def produce(self, broker: Type[Broker], *args, **kwargs):
+    async def produce(
+        self, broker: Type[Broker], *args, timeout: Optional[Number] = None, **kwargs
+    ):
         raise NotImplementedError
+
+
+class TimeCounterProducer(Producer):
+    def __init__(
+        self,
+        name: Text = "TimeCounterProducer",
+        timeout: Optional[Number] = None,
+        count_seconds: float = 1.0,
+        max_task_count: Optional[int] = None,
+        put_value: Any = 1,
+        *args,
+        **kwargs,
+    ):
+        super(TimeCounterProducer, self).__init__(
+            name=name, *args, timeout=timeout, **kwargs
+        )
+        self.count_seconds = count_seconds
+        self.max_task_count = max_task_count
+        self.put_value = put_value
+
+    async def produce(
+        self,
+        broker: Type[Broker],
+        *args,
+        timeout: Optional[Number] = None,
+        count_seconds: Optional[float] = None,
+        max_task_count: Optional[int] = None,
+        put_value: Any = None,
+        ignore_full_error: bool = False,
+        raise_full_error: bool = False,
+        **kwargs,
+    ):
+        timeout = timeout or self.timeout
+        count_seconds = self.count_seconds if count_seconds is None else count_seconds
+        max_task_count = max_task_count or self.max_task_count or float("inf")
+        put_value = put_value or self.put_value
+
+        count = 0
+        while True:
+            try:
+                await broker.put_nowait(put_value)
+            except FullError as e:
+                if raise_full_error is True:
+                    raise e
+                if ignore_full_error is True:
+                    logger.info(
+                        f"The broker '{broker}' is full, "
+                        + f"so skip the item '{put_value}'."
+                    )
+                else:
+                    logger.error(
+                        f"The broker '{broker}' is full, "
+                        + f"so skip the item '{put_value}'."
+                    )
+
+            count += 1
+            if count >= max_task_count:
+                break
+
+            await asyncio.sleep(count_seconds)
