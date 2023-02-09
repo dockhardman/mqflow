e0ad1ea7051e80f33d484ce9f1853ab6b14ac2f5
Allen Chou <f1470891079@gmail.com>
2023-02-09 18:54:41 +0800

    Basic consumer with testing.

--- simple_pymq/__init__.py
+++ simple_pymq/__init__.py
@@ -1,12 +1,15 @@
-from .version import version
 from .broker.base import Broker, QueueBroker
+from .consumer.base import Consumer, PrintConsumer
 from .producer.base import Producer, TimeCounterProducer
+from .version import version
 
 
 __version__ = version
 
 __all__ = [
     "Broker",
+    "Consumer",
+    "PrintConsumer",
     "Producer",
     "QueueBroker",
     "TimeCounterProducer",
---
+++ tests/consumer/test_base.py
@@ -0,0 +1,24 @@
+import asyncio
+
+import pytest
+
+from simple_pymq import PrintConsumer, QueueBroker
+
+
+@pytest.mark.parametrize("item_count,consume_count", [(4, 2), (2, 2)])
+@pytest.mark.asyncio
+async def test_consumer_basic_operation(item_count: int, consume_count: int):
+    q = QueueBroker(maxsize=10)
+    c = PrintConsumer()
+
+    task = asyncio.create_task(c.listen(broker=q, max_consume_count=consume_count))
+
+    for _ in range(item_count):
+        await q.put_nowait(True)
+    await task
+
+    # Check consumer has consumed item.
+    if item_count > consume_count:
+        assert await q.qsize() == (item_count - consume_count)
+    else:
+        assert await q.qsize() == 0
