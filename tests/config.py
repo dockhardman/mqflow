20d69e54538427d558fd141a0f69e092ff776a7e
Allen Chou <f1470891079@gmail.com>
2023-02-22 14:26:14 +0800

    Add pytest redis config.

--- tests/config.py
+++ tests/config.py
@@ -1,12 +1,35 @@
+import logging
 import uuid
+from typing import Text
+
+from rich.console import Console
 
 from simple_pymq.config import get_os_env
 
 
 class Settings:
+    # General Config
+    logger_name: Text = "pytest"
     test_uuid: uuid.UUID = get_os_env(
         "test_uuid", default=uuid.uuid4(), return_type="uuid"
     )
 
+    # Redis Config
+    test_redis_host: Text = get_os_env(
+        "test_redis_host", default=None, return_type="str"
+    )
+    test_redis_port: int = get_os_env(
+        "test_redis_port", default=None, return_type="int"
+    )
+    test_redis_db: int = get_os_env("test_redis_db", default=None, return_type="int")
+    test_redis_username: Text = get_os_env(
+        "test_redis_username", default=None, return_type="str"
+    )
+    test_redis_password: Text = get_os_env(
+        "test_redis_password", default=None, return_type="str"
+    )
+
 
 settings = Settings()
+console = Console()
+logger = logging.getLogger(settings.logger_name)
