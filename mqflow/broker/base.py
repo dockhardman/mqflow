d6d9a3be6f693f177daafc64cce4fad165178443
Allen Chou <f1470891079@gmail.com>
2023-06-03 12:48:55 +0800

    Refactor QueueBroker.

--- mqflow/broker/base.py
+++ mqflow/broker/base.py
@@ -1,128 +1,162 @@
-import asyncio
 from abc import ABC
 from numbers import Number
-from typing import Any, Optional, Text
+from queue import Queue, Empty as QueueEmpty, Full as QueueFull
+from typing import Generic, Optional, Text, TypeVar
 
 from mqflow.exceptions import FullError, EmptyError
 
 
-class Broker(ABC):
+T = TypeVar("T")
+
+
+class BrokerBase(ABC, Generic[T]):
     def __init__(
         self,
-        name: Text = "Broker",
         maxsize: int = 0,
         *args,
+        name: Text = "BrokerBase",
         block: bool = True,
         timeout: Optional[Number] = None,
-        **kwargs
+        **kwargs,
     ):
         self.name = name
         self.maxsize = maxsize
         self.block = block
         self.timeout = timeout
 
-    async def empty(self) -> bool:
+    def __repr__(self) -> Text:
+        return f"{self.__class__.__name__}(name={self.name}, maxsize={self.maxsize})"
+
+    def __str__(self) -> Text:
+        return self.__repr__()
+
+    def __len__(self) -> int:
+        return self.qsize()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            return self.get_nowait()
+        except EmptyError:
+            raise StopIteration
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    def empty(self) -> bool:
         raise NotImplementedError
 
-    async def full(self) -> bool:
+    def full(self) -> bool:
         raise NotImplementedError
 
-    async def get(self, block: bool = True, timeout: Optional[Number] = None) -> Any:
+    def get(self, block: Optional[bool] = None, timeout: Optional[Number] = None) -> T:
         raise NotImplementedError
 
-    async def get_nowait(self) -> Any:
+    def get_nowait(self) -> T:
         raise NotImplementedError
 
-    async def join(self) -> None:
+    def join(self) -> None:
         raise NotImplementedError
 
-    async def put(
-        self, item: Any, block: bool = True, timeout: Optional[Number] = None
+    def put(
+        self, item: T, block: Optional[bool] = None, timeout: Optional[Number] = None
     ) -> None:
         raise NotImplementedError
 
-    async def put_nowait(self, item: Any) -> None:
+    def put_nowait(self, item: T) -> None:
         raise NotImplementedError
 
-    async def qsize(self) -> int:
+    def qsize(self) -> int:
         raise NotImplementedError
 
-    async def task_done(self) -> None:
+    def task_done(self) -> None:
         raise NotImplementedError
 
-    async def close(self) -> None:
+    def close(self) -> None:
         pass
 
 
-class QueueBroker(Broker):
+class QueueBroker(BrokerBase[T]):
     def __init__(
         self,
-        name: Text = "QueueBroker",
         maxsize: int = 0,
+        *args,
+        name: Text = "QueueBroker",
         block: bool = True,
         timeout: Optional[Number] = None,
-        loop: Optional[asyncio.AbstractEventLoop] = None,
-        *args,
-        **kwargs
+        queue: Optional["Queue[T]"] = None,
+        **kwargs,
     ):
-        super(QueueBroker, self).__init__(
-            name=name, maxsize=maxsize, *args, block=block, timeout=timeout, **kwargs
+        super().__init__(
+            maxsize, *args, name=name, block=block, timeout=timeout, kwargs=kwargs
         )
-        self.name = name
-        self.maxsize = maxsize
-        self.queue = asyncio.Queue(maxsize=maxsize, loop=loop)
 
-    async def empty(self) -> bool:
+        self.queue = queue or Queue(maxsize=maxsize)
+        self.maxsize = self.queue.maxsize
+
+    def empty(self) -> bool:
         return self.queue.empty()
 
-    async def full(self) -> bool:
+    def full(self) -> bool:
         return self.queue.full()
 
-    async def get(
-        self, block: Optional[bool] = None, timeout: Optional[Number] = None
-    ) -> Any:
-        block = self.block if block is None else block
-        timeout = self.timeout if timeout is None else timeout
+    def get(self, block: Optional[bool] = None, timeout: Optional[Number] = None) -> T:
+        block = block if block is None else self.block
+        timeout = timeout if timeout is None else self.timeout
 
-        if block is True:
-            item = await asyncio.wait_for(self.queue.get(), timeout=timeout)
-        else:
-            item = self.queue.get_nowait()
-
-        return item
-
-    async def get_nowait(self) -> Any:
         try:
-            item = self.queue.get_nowait()
-            return item
-        except asyncio.QueueEmpty:
-            raise EmptyError
+            return self.queue.get(block=block, timeout=timeout)
+        except QueueEmpty as e:
+            raise EmptyError(e)
+        except TimeoutError as e:
+            raise e
+        except Exception as e:
+            raise e
 
-    async def join(self) -> None:
-        await self.queue.join()
+    def get_nowait(self) -> T:
+        try:
+            return self.queue.get_nowait()
+        except QueueEmpty as e:
+            raise EmptyError(e)
+        except Exception as e:
+            raise e
 
-    async def put(
-        self, item: Any, block: Optional[bool] = None, timeout: Optional[Number] = None
-    ) -> None:
-        block = self.block if block is None else block
-        timeout = self.timeout if timeout is None else timeout
+    def join(self) -> None:
+        self.queue.join()
 
-        if block is True:
-            await asyncio.wait_for(self.queue.put(item), timeout=timeout)
-        else:
-            try:
-                self.queue.put_nowait(item)
-            except asyncio.QueueFull:
-                raise FullError
+    def put(
+        self, item: T, block: Optional[bool] = None, timeout: Optional[Number] = None
+    ):
+        block = block if block is None else self.block
+        timeout = timeout if timeout is None else self.timeout
 
-    async def put_nowait(self, item: Any):
+        try:
+            self.queue.put(item, block=block, timeout=timeout)
+        except QueueFull as e:
+            raise FullError(e)
+        except TimeoutError as e:
+            raise e
+        except Exception as e:
+            raise e
+
+    def put_nowait(self, item: T) -> None:
         try:
             self.queue.put_nowait(item)
-        except asyncio.QueueFull:
-            raise FullError
+        except QueueFull as e:
+            raise FullError(e)
+        except Exception as e:
+            raise e
 
-    async def qsize(self) -> int:
+    def qsize(self) -> int:
         return self.queue.qsize()
 
-    async def task_done(self) -> None:
+    def task_done(self) -> None:
         self.queue.task_done()
+
+    def close(self) -> None:
+        pass
